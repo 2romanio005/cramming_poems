39dd6ea0e1a0b17915e115a6af6ded4a2bfbccae
Roman_Proskuryakov <2romanio005@gmail.com>
2024-01-18 15:19:45 +0300

    move mainThem to styles.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cramming_poems/future_builder.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:cramming_poems/styles.dart';
 
-import 'colors.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,32 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const FutureBuilderHome(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: ColorPrimary,
-          brightness: Brightness.light,
-        ),
-        textTheme: TextTheme(
-          // TODO: Выбрать нормальные шрифты и размер текста
-          titleLarge: GoogleFonts.oswald(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-          bodyLarge: GoogleFonts.oswald(
-            // rubikMonoOne
-            // quantico
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-          ),
-          bodyMedium: GoogleFonts.robotoMono(
-            // anonymousPro
-            fontSize: 16,
-          ),
-          bodySmall:  GoogleFonts.robotoMono(
-            fontSize: 10,
-          ),
-        ),
-      ),
-    );
+      theme: mainTheme,
   }
 }
--- lib/styles.dart
+++ lib/styles.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:cramming_poems/colors.dart';
+
+ThemeData mainTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: ColorPrimary,
+    brightness: Brightness.light,
+  ),
+  textTheme: TextTheme(
+    // TODO: Выбрать нормальные шрифты и размер текста
+    titleLarge: GoogleFonts.oswald(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyLarge: GoogleFonts.oswald(
+      // rubikMonoOne
+      // quantico
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyMedium: GoogleFonts.robotoMono(
+      // anonymousPro
+      fontSize: 16,
+    ),
+    bodySmall:  GoogleFonts.robotoMono(
+      fontSize: 10,
+    ),
+  ),
+);
 
 ButtonStyle buttonStyleOFF = IconButton.styleFrom(
   backgroundColor: ColorButtonBackground,
